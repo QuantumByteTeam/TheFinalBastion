140a54960c190ef66dc14a28b6619e742253f9c9
JohnMcFarland <john.e.mcfarland2000@gmail.com>
2023-12-01 21:28:29 -0500

    Main UI components added an updated GameManager is needed to allow certain compenents to be connected to their respective values (Win/Lose Conditions, Player HP, Point HP, Enemies, and Waves) Until then, only the Pause menu works. Nothing else can be tested atm The Game Manager is also the one managing the Pause/Win/Lose menus anyway

--- FPSProject_Team4/Assets/Scripts/UIManager.cs
+++ FPSProject_Team4/Assets/Scripts/UIManager.cs
@@ -1,54 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager instance;
     public GameObject player;
-    [SerializeField] GameObject HPText;
+    [SerializeField] TMP_Text HPText;
     [SerializeField] GameObject PointHPBar;
-
     [SerializeField] GameObject menuActive;
     [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject point;
+
+    [SerializeField] Image pointHPImage;
 
     float timeScale;
     public bool isPaused;
 
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         timeScale= Time.timeScale;
         instance= this;
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameManager.instance.player;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("Cancel") && menuActive == null)
-        {
-            statePaused();
-            menuActive = menuPause;
-            menuActive.SetActive(isPaused);
-        }
+        //need to access the HP of the point
+        UIManager.instance.pointHPImage.fillAmount = point.HP / point.HPOriginal;
+
+        //need to update the HP integer label with the HP of the player, a bar is not used, but a label, i cannot access the player HP from here
+
+        //Might want to update these labels (wave and enemy included) not on every frame
     }
 
-    public void statePaused()
+
+    public void Resume()
     {
-        isPaused = !isPaused;
-        Time.timeScale = 0;
-        Cursor.visible = true;
-        Cursor.lockState = CursorLockMode.Confined;
+        GameManager.instance.StateUnpaused();
     }
 
-    public void stateUnpaused()
+    public void restart()
     {
-        isPaused = !isPaused;
-        Time.timeScale = timeScale;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        menuActive.SetActive(false);
-        menuActive = null;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        GameManager.instance.StateUnpaused();
+    }
+
+    public void quit()
+    {
+        Application.Quit();
     }
 }
