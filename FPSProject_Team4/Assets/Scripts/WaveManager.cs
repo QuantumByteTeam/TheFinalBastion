b91a6cd42af33b701dd5d750e3cb8617250af95a
Richie Chao <110568947+fs-richie@users.noreply.github.com>
2023-12-12 18:00:49 -0800

    Wave Manager update
    • Updated the Wave Manager to allow spawning of multiple enemy types.
    • First wave should spawn only the base count amount
    • Updated the Spawners so that the enemies don't spawn inside one another
         • Enemies should spawn in a certain radius around the Spawner

--- FPSProject_Team4/Assets/Scripts/WaveManager.cs
+++ FPSProject_Team4/Assets/Scripts/WaveManager.cs
@@ -6,12 +6,18 @@ public class WaveManager : MonoBehaviour
 {
     public static WaveManager instance;
 
+    [Header("----- Spawners -----")]
     [SerializeField] GameObject[] spawners;
+    
+    [Header("----- Enemies -----")]
+    // [SerializeField] GameObject enemy;
+    [SerializeField] GameObject[] enemy;
     [SerializeField] int baseEnemyCount;
-    [SerializeField] int waveIntermission;
     [SerializeField] float enemyCountMultiplier;
+    
+    [Header("----- Wave -----")]
+    [SerializeField] int waveIntermission;
     [SerializeField] int waveWinAmount;
-    [SerializeField] GameObject enemy;
 
     bool intermission;
     
@@ -42,10 +48,29 @@ public class WaveManager : MonoBehaviour
             }
 
             // TODO: Need to fix caculation here
-            for (int i = 0; i < baseEnemyCount * enemyCountMultiplier * currentWave; i++)
+            // for (int i = 0; i < baseEnemyCount * enemyCountMultiplier * currentWave; i++)
+            // {
+                // GameObject randSpawner = spawners[Random.Range(0, spawners.Length)];
+                // Instantiate(enemy, randSpawner.transform.position, randSpawner.transform.rotation);
+            // }
+
+            if (currentWave == 0 || currentWave == 1)
             {
-                GameObject randSpawner = spawners[Random.Range(0, spawners.Length)];
-                Instantiate(enemy, randSpawner.transform.position, randSpawner.transform.rotation);
+                for (int i = 0; i < baseEnemyCount * currentWave; i++) // First wave should always spawn base enemy count
+                {
+                    GameObject randSpawner = spawners[Random.Range(0, spawners.Length)]; // Get a random spawner
+                    Vector3 randSpawnerPoint = randSpawner.transform.position + Random.insideUnitSphere * 5; // find a point in a sphere around the spawner
+                    Instantiate(enemy[Random.Range(0, enemy.Length)], randSpawnerPoint, randSpawner.transform.rotation); // Spawns a random enemy type on the random point
+                }
+            }
+            else
+            {
+                for (int i = 0; i < baseEnemyCount * enemyCountMultiplier * currentWave; i++)
+                {
+                    GameObject randSpawner = spawners[Random.Range(0, spawners.Length)]; // Get a random spawner
+                    Vector3 randSpawnerPoint = randSpawner.transform.position + Random.insideUnitSphere * 5; // find a point in a sphere around the spawner
+                    Instantiate(enemy[Random.Range(0, enemy.Length)], randSpawnerPoint, randSpawner.transform.rotation); // Spawns a random enemy type on the random point
+                }
             }
         }
     }
