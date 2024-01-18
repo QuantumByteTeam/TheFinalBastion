0ed2de4fb81870c7ede29073dc1809a78ce08d20
PGJacobSE <JSEndsley@student.fullsail.edu>
2024-01-18 14:27:02 -0600

    Power ups/Loot drop update

---
+++ FPSProject_Team4/Assets/Scripts/dmgPickUp.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[CreateAssetMenu(menuName = "PowerUps/DmgBuff")]
+
+/*public class dmgPickUp : powerUpEffect
+{
+    public int amount;
+
+    public override void Apply(GameObject target)
+    {
+        target.GetComponent<gunStats>().ShootDamage *= amount;
+    }
+}*/
