478b0e39d5354e8f4936cbeb25aead85fa74787a
Julius Villamar-Del Carmen <crims0nt00nlink2@gmail.com>
2024-01-18 21:34:55 -0700

    Crafting System Done
    Base crafting system done, need to implement it into interact key

---
+++ FPSProject_Team4/Assets/Scripts/CraftingSystem/Crafting Slot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CraftingSlot : MonoBehaviour
+{
+    public CraftingItem item; //stores the item inside the slot
+    public int index; //stores the item into an index, used for recipe system
+
+
+}
