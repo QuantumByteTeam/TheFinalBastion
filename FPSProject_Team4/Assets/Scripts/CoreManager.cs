190d65155bf94869d69d7a864c9179ff84cabcf6
Tristan Ernst <60721601+Tanoflame@users.noreply.github.com>
2024-01-19 23:02:43 -0500

    Added CoreManager, uncomment needed after core is created
    Added CoreManager

---
+++ FPSProject_Team4/Assets/Scripts/CoreManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoreManager : MonoBehaviour
+{
+    public static CoreManager instance;
+    public List<GameObject> cores;
+
+    void Start()
+    {
+        instance = this;
+    }
+
+    public void RegisterCore(GameObject obj)
+    {
+        //if (obj.GetComponent<Core>()) // TODO: Uncomment this check once Core is implemented
+        {
+            cores.Add(obj);
+        }
+    }
+
+    public List<GameObject> GetAllCores()
+    {
+        return cores;
+    }
+
+    public GameObject GetClosestToPosition(Vector3 position)
+    {
+        GameObject closestObj = null;
+        float closestDist = float.MaxValue;
+
+        foreach (GameObject obj in cores)
+        {
+            float distance = Vector3.Distance(obj.transform.position, position);
+
+            if (distance < closestDist)
+            {
+                closestObj = obj;
+                closestDist = distance;
+            }
+        }
+
+        return closestObj;
+    }
+}
